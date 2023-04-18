01ac4113472ef5aa947dd3b831a239bf6fa384af
martinmwas254 <martinmwas264@gmail.com>
2023-04-18 18:38:16 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
-
+import Mile from "./test.js"     
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Mile/>
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,9 @@
+import React from "react"
+export default function Mile(){
+    return (
+        < >
+        <img src="https://media4.giphy.com/media/3ohhwfAa9rbXaZe86c/200w.gif" alt="skeleton"/>
+        <p>insider</p>
+      </>
+    )
+}
